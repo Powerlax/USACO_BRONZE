03a310472892f0c37d62573b8f33a06fdcaea126
Powerlax <raovishruth@gmail.com>
2023-07-17 18:04:42 -0700

    Did Distinct Numbers

--- src/Distinct.java
+++ src/Distinct.java
@@ -1,14 +1,12 @@
-import java.io.File;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
-import java.util.TreeSet;
 
 public class Distinct {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        Set<Integer> data = new java.util.HashSet<>();
+        Set<Integer> data = new HashSet<>();
         for (int i = 0; i<n; i++) {
             data.add(scanner.nextInt());
         }
