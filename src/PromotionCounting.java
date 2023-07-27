c7a22c250e37e68af1be5c63589113096e4a5cd7
Powerlax <raovishruth@gmail.com>
2023-07-27 16:33:58 -0700

    Did Promotion Counting

---
+++ src/PromotionCounting.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class PromotionCounting {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("promote.in"));
+        int[] before = new int[4];
+        int[] after = new int[4];
+        for (int i = 0; i<4; i++) {
+            before[i] = scanner.nextInt();
+            after[i] = scanner.nextInt();
+        }
+        int GtoP = after[3] - before[3];
+        int StoG = after[2] - (before[2] + GtoP);
+        int BtoS = after[1] - (before[1] + StoG + GtoP);
+        PrintWriter pw = new PrintWriter("promote.out");
+        pw.println(BtoS);
+        pw.println(StoG);
+        pw.println(GtoP);
+    }
+}
